8531c38b4d260255f814a473c950435e26dd087e
thuan <pham.t.286.01@gmail.com>
2024-09-10 23:50:50 +0900

    remove s3 services

--- src/config/s3.js
+++ src/config/s3.js
@@ -1,28 +1,28 @@
-const multerS3 = require('multer-s3');
-const { S3Client } = require('@aws-sdk/client-s3');
-const config = require('./config');
+// const multerS3 = require('multer-s3');
+// const { S3Client } = require('@aws-sdk/client-s3');
+// const config = require('./config');
 
-const s3 = new S3Client({
-  credentials: {
-    accessKeyId: config.aws.accessKeyId,
-    secretAccessKey: config.aws.secretAccessKey,
-  },
-  region: config.aws.region,
-});
+// const s3 = new S3Client({
+//   credentials: {
+//     accessKeyId: config.aws.accessKeyId,
+//     secretAccessKey: config.aws.secretAccessKey,
+//   },
+//   region: config.aws.region,
+// });
 
-const s3Storage = multerS3({
-  s3, // s3 instance
-  bucket: 'my-images', // change it as per your project requirement
-  acl: 'public-read', // storage access type
-  metadata: (req, file, cb) => {
-    cb(null, { fieldName: file.fieldName });
-  },
-  key: (req, file, cb) => {
-    const fileName = `${Date.now()}_${file.fieldName}_${file.originalname}`;
-    cb(null, fileName);
-  },
-});
+// const s3Storage = multerS3({
+//   s3, // s3 instance
+//   bucket: 'my-images', // change it as per your project requirement
+//   acl: 'public-read', // storage access type
+//   metadata: (req, file, cb) => {
+//     cb(null, { fieldName: file.fieldName });
+//   },
+//   key: (req, file, cb) => {
+//     const fileName = `${Date.now()}_${file.fieldName}_${file.originalname}`;
+//     cb(null, fileName);
+//   },
+// });
 
-module.exports = {
-  s3Storage,
-};
+// module.exports = {
+//   s3Storage,
+// };
